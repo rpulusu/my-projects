7913a8117fb192d92deeea095b1314a3438d8f94
rpulusu <35186443+rpulusu@users.noreply.github.com>
2018-05-15 12:39:51 -0500

    Add files via upload
    Basic SpringRestServices application

---
+++ SpringRestServices/src/main/java/com/abc/gate/ApplicationConfiguration.java
@@ -0,0 +1,11 @@
+package com.abc.gate;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.abc.gate")
+public class ApplicationConfiguration {
+	
+}
---
+++ SpringRestServices/src/main/java/com/abc/gate/PaymentRequest.java
@@ -0,0 +1,29 @@
+package com.abc.gate;
+
+public class PaymentRequest {
+
+	private int userId;
+	private String itemId;
+	private double discount;
+	
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getItemId() {
+		return itemId;
+	}
+	public void setItemId(String itemId) {
+		this.itemId = itemId;
+	}
+	public double getDiscount() {
+		return discount;
+	}
+	public void setDiscount(double discount) {
+		this.discount = discount;
+	}
+	
+	
+}
